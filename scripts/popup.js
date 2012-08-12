b3d6be9d1e65c08ff0c3ef4095260b860aaa7a91
TimZhang <timzhang@dnspod.com>
2012-08-13 01:30:08 +0800

    version 0.1

---
+++ scripts/popup.js
@@ -0,0 +1,31 @@
+$(function(){
+	_.mixin(_.str.exports()); // Include Underscore.string methods to Underscore name
+	var API_SEARCH = "http://cloud.rollingword.com/api/word/" ;
+	$('form').submit(function(e){
+		e.preventDefault();
+		var word = $('#word').val();
+		var source = $("#tpl-word-def").html();
+		var template = Handlebars.compile(source);
+		if (_.trim(word).length == 0) { //Search empty 
+			$('#result').html(template({info: '好像少了点什么'}));
+			return;
+		}
+		$.ajax({
+			url: API_SEARCH + encodeURI(word),
+			dataType:'json',
+			success:function(data){
+				if(data.status == "OK") {
+					$('#result').html(template(_.extend(data, {word: word})));
+					// $('#result').html(Mustache.render(_.extend(data, {word: word})));
+
+
+				} else {
+					$('#result').html(template(_.extend(data, {info: '翻滚吧，牛宝宝！'})));
+
+				}
+
+			},
+		});
+
+	});
+});
